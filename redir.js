8521c58842f23477fd45b450f434cf1599bfd88c
Kelvin Santos <23528635+santoskelvin@users.noreply.github.com>
2024-07-07 03:17:46 -0300

    atualizacao

---
+++ redir.js
@@ -0,0 +1,19 @@
+       
+       // Função para redirecionar para o link atual do span
+       function redirecionarParaLink() {
+        var link = document.getElementById('paybutton').getAttribute('href');
+        if (link) {
+            window.location.href = link;
+        }
+    }
+
+    // Função para mudar dinamicamente o link
+    function alterarLinkNovo(linkNovo) {
+        var botao = document.getElementById('paybutton');
+        if (botao) {
+            botao.setAttribute('href', linkNovo);
+        }
+    }
+
+    // Alterar dinamicamente para o novo link
+    alterarLinkNovo('https://pay.processarpagamento.store/JqoR32AqM5agVj5');
